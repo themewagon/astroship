fb6926d40851b669c601c0712d90c1580e8ab860
Pipul Boddyo <pipul.boddyo@gmail.com>
2026-08-04 16:28:43 +0600

    release v1.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://themewagon.com",
+  base: '/astroship',
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
